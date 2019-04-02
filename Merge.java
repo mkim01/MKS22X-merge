a7f86ce9eec7de79f719160cd1a0dd9cb82241e7
Minuk Kim <mkim01@stuy.edu>
2019-04-02 06:05:40 -0400

    change the paramter of merge method and fix mergesort

--- Merge.java
+++ Merge.java
@@ -1,34 +1,21 @@
 import java.util.*;
 public class Merge{
 
-  public static void merge(int[]data, int lo, int mid, int hi){
-   //System.out.println(Arrays.toString(data));
-    int lsize = mid - lo + 1;
-    int rsize = hi - mid;
-    //assign value to temp data set;
-    int[] left = new int[lsize];
-    for (int i = 0; i < lsize; i++){
-      left[i] = data[i];
-    }
-    int[] right = new int[rsize];
-    for (int i = 0; i < rsize; i++){
-      right[i] = data[rsize + i];
-    }
-    // System.out.println(Arrays.toString(left));
-    // System.out.println(Arrays.toString(right));
+  public static void merge(int[]data, int[] left, int[] right){
 
     int i = 0; //index of original data as you copy down
     int r = 0; //right array index
     int l = 0; //left array index
-
+    int rsize = right.length;
+    int lsize = left.length;
     while (r < rsize && l < lsize){
-      if(right[r] <= left[l]){
-        data[i] = right[r];
-        r++;
-      }
-      else{
+      if(right[r] > left[l]){
         data[i] = left[l];
         l++;
+      }
+      else{
+        data[i] = right[r];
+        r++;
       }
       i++;
     }
@@ -45,17 +32,30 @@ public class Merge{
     }
     // System.out.println(Arrays.toString(data));
     return ;
-}
+  }
 
   public static void mergesort(int[] data, int lo, int hi){
     if (lo >= hi){
-      return ;
+      return;
     }
-      int mid = (hi + lo) / 2;
-      mergesort(data, lo ,mid);
-      mergesort(data, mid + 1 , hi);
-      merge(data, lo, mid, hi);
-  }
+
+      int mid = data.length/2;
+     //System.out.println(Arrays.toString(data));
+      int lsize = mid;
+      int rsize = data.length - mid;
+      //assign value to temp data set;
+      int[] left = new int[lsize];
+      for (int i = 0; i < lsize; i++){
+        left[i] = data[i];
+      }
+      int[] right = new int[rsize];
+      for (int i = 0; i < rsize; i++){
+        right[i] = data[mid + i];
+      }
+      mergesort(left, 0, left.length-1);
+      mergesort(right,0, right.length - 1);
+      merge(data,left,right);
+    }
 
   public static void mergesort(int[] data){
     mergesort(data, 0, data.length - 1);
@@ -75,7 +75,7 @@ public class Merge{
 
   public static void main(String[] args){
     int[] test= {5,3,2,4,1};
-    mergesort(test,0, test.length-1);
+    mergesort(test);
 
   }
 
